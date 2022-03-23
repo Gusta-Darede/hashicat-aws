4e647f8ef2d7aedcd0137c2e8f2f8542a78b5dd7
Gustavo Oliveira do Nascimento <gustavo.oliveira@darede.com.br>
2022-03-23 07:23:59 +0000

    Added s3 bucket module - v4

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket-hashcatGustaoliveira"
+#  bucket = "my-s3-bucket-hashcatGustaoliveira"
   acl    = "private"
 
   versioning = {
